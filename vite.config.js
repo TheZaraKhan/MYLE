30d70f8db63d1bf7bb0ef4ffc02d6e96c15e8604
Zara Khan <ZaraMustafa@ymail.com>
2022-02-22 23:08:44 +0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -35,6 +35,7 @@ module.exports = {
         userprofile: resolve(__dirname, "pages/userprofile/index.html"),
         reviews: resolve(__dirname, "pages/reviews/index.html"),
         checkout: resolve(__dirname, "pages/checkout/index.html"),
+        success: resolve(__dirname, "pages/success/index.html"),
       },
     },
   },
